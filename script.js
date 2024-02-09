fc07785f34831c75a9342e47144f66370b12aa79
arun-rai14 <148248176+arun-rai14@users.noreply.github.com>
2024-02-09 02:42:09 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+let noBtnClickedCount = 0;
+
+document.getElementById('noBtn').addEventListener('click', function() {
+    let yesBtn = document.getElementById('yesBtn');
+    let message = document.getElementById('message');
+    let additionalImage = document.getElementById('additionalImage');
+    noBtnClickedCount++;
+    
+    let scaleValue = 1 + noBtnClickedCount * 1;
+    yesBtn.style.transform = `scale(${scaleValue})`;
+    yesBtn.classList.add('green-button');
+
+    if (noBtnClickedCount === 1) {
+        message.textContent = "Are you actually sure?";
+        additionalImage.style.display = "block"; // Show the first image if it's the first click
+    } else if (noBtnClickedCount === 2) {
+        message.textContent = "Oh, playing hard to get? 😏";
+        additionalImage.src = "hard.jpg";
+        additionalImage.alt = "Hard Image";
+    } else if (noBtnClickedCount === 3) {
+        message.textContent = "Common Seriously!?";
+        additionalImage.src = "Seriously.jpg";
+        additionalImage.alt = "Serious Image";
+    } else {
+        message.textContent = "Just kidding, we know you meant yes!";
+        additionalImage.src = "kidding.jpg";
+        additionalImage.alt = "Kidding Image";
+    }
+});
+
+document.getElementById('yesBtn').addEventListener('click', function() {
+    let message = document.getElementById('message');
+    let additionalImage = document.getElementById('additionalImage');
+
+    message.textContent = "YAY! 🎉 You have officially been booked with Arun for 18th of February.";
+    additionalImage.src = "YAY.jpg"; // Display this image when "Yes" is clicked
+    additionalImage.alt = "YAY Image"; // Update alt attribute accordingly
+    additionalImage.style.display = "block"; // Make sure the image is visible
+
+    removeButtons();
+});
+
+function removeButtons() {
+    let container = document.querySelector('.container');
+    let buttonsContainer = document.querySelector('.buttons-container');
+    if (buttonsContainer) {
+        container.removeChild(buttonsContainer);
+    }
+}
